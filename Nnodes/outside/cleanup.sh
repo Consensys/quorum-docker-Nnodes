cdb514401ab2b63de009d8d83a97166a2a810631
Power Li <pccr10001@gmail.com>
2019-02-13 20:20:19 +0800

    fix cleanup

--- Nnodes/outside/cleanup.sh
+++ Nnodes/outside/cleanup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-if [[ -e config.sh ]]; then
-    source config.sh
-    rm -rf $node_name_prefix-$service 2>/dev/null
-fi
+echo "  - Removing containers."
+docker-compose --log-level ERROR down 2>/dev/null
 
 echo "  - Removing old data."
-rm -rf qdata_[0-9] qdata_[0-9][0-9]
-rm -f docker-compose.yml
-rm -f contract_pri.js contract_pub.js
-rm .current_config 2>/dev/null
-rm genesis.json 2>/dev/null
-
-# Shouldn't be needed, but just in case:
-rm -f static-nodes.json genesis.json
+rm -rf qdata_[0-9]/dd/geth qdata_[0-9][0-9]/dd/geth
