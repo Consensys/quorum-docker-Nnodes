a36657b7bcc2d46c81b78b6e4bd21812d6dd5611
Power Li <pccr10001@gmail.com>
2019-02-21 17:24:42 +0800

    add docker host network support

--- Nnodes/outside/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "  - Removing containers."
-docker-compose --log-level ERROR down 2>/dev/null
-
-echo "  - Removing old data."
-rm -rf qdata_[0-9]/dd/geth qdata_[0-9][0-9]/dd/geth
